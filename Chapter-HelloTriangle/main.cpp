1a107a7f8596e19214b9c0beac472f9334ea540f
Almir Neto <almirneto338@gmail.com>
2021-08-22 19:49:44 -0300

    Setting HelloTriangle chapter folder

---
+++ Chapter-HelloTriangle/main.cpp
@@ -0,0 +1,52 @@
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+#include <iostream>
+
+void framebuffer_resize(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+int main(int argc, char const *argv[])
+{
+    if (glfwInit() != GLFW_TRUE)
+    {
+        std::cout << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+    // Set the OpenGL context version
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // Disable window resize button
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Learn OpenGL", NULL, NULL);
+    if (!window)
+    {
+        std::cout << "Unable to create Window" << std::endl;
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    // Set the Open GL viewport
+    glViewport(0, 0, 800, 600);
+    // Set the clear color
+    glClearColor(.3f, .3f, .3f, 1.f);
+    glfwSetFramebufferSizeCallback(window, framebuffer_resize);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        // Clear window
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
