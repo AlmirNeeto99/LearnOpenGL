ce9930ce24f153d916bf92f475a17a281e233fdf
Almir Neto <almirneto338@gmail.com>
2021-08-23 15:28:33 -0300

    ✨ Creating two textures

---
+++ Chapter-TwoTextures/main.cpp
@@ -0,0 +1,171 @@
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <cmath>
+#include "../src/headers/Shader.hpp"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+void framebuffer_resize(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+// Triangle's vertices
+GLfloat vertices[] = {
+    -.5f, .5f, .0f, .3f, .5f, .1f, .0f, 1.f,     // Top left
+    .5f, .5f, .0f, .6f, .6f, .9f, 1.f, 1.f,      // Top right
+    .5f, -.5f, .0f, .9f, .75f, .15f, 1.f, .0f,   // Bottom right
+    -.5f, -.5f, .0f, .15f, .3f, .84f, .0f, .0f}; // Bottom left
+
+unsigned int indexes[] = {
+    0, 1, 3, // First triangle
+    1, 2, 3  // Second triangle
+};
+
+int main(int argc, char const *argv[])
+{
+    if (glfwInit() != GLFW_TRUE)
+    {
+        std::cout << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+    // Set the OpenGL context version
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // Disable window resize button
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL - Two Textures", NULL, NULL);
+    if (!window)
+    {
+        std::cout << "Unable to create Window" << std::endl;
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    // Set the Open GL viewport
+    glViewport(0, 0, 800, 600);
+    // Set the clear color
+    glClearColor(.3f, .3f, .3f, 1.f);
+    glfwSetFramebufferSizeCallback(window, framebuffer_resize);
+    Shader *shader = NULL;
+    try
+    {
+        shader = new Shader("../resources/shaders/textures/vertex.vert", "../resources/shaders/two-textures/fragment.frag");
+    }
+    catch (const std::exception &ex)
+    {
+        std::cerr << ex.what() << std::endl;
+        return -1;
+    }
+
+    unsigned int VAO;
+    // Create a new VertexArrayBuffer
+    glGenVertexArrays(1, &VAO);
+
+    unsigned int VBO;
+    // Generate one buffer
+    glGenBuffers(1, &VBO);
+
+    unsigned int EBO;
+    // Generate one buffer
+    glGenBuffers(1, &EBO);
+
+    // Bind the VertexArrayBuffer
+    glBindVertexArray(VAO);
+    // Bind GL_ARRAY_BUFFER to VBO
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // Copy the vertices data to GPU
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // Bind GL_ELEMENT_ARRAY_BUFFER to EBO
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    // Copy the indexes to GPU
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indexes), indexes, GL_STATIC_DRAW);
+    // Set the attributes of the vertices
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void *)0);
+    // Enable the attribute at position 0
+    glEnableVertexAttribArray(0);
+    // Set vertex color attributes
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void *)(3 * sizeof(GLfloat)));
+    // Enable the attribute at position 1
+    glEnableVertexAttribArray(1);
+    // Set texture coordinates attributes
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void *)(6 * sizeof(GLfloat)));
+    // Enable the attribute at position 2
+    glEnableVertexAttribArray(2);
+    // Variables to keep imagem information
+    int width, height, nrChannels;
+    // Load wall texture
+    unsigned char *wall = stbi_load("../resources/textures/wall.jpg", &width, &height, &nrChannels, 0);
+
+    unsigned int texture1, texture2;
+    glGenTextures(1, &texture1);
+    glGenTextures(1, &texture2);
+    // Set TEXTURE_0 current active texture
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, wall);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    // Unbind texture
+    glBindTexture(GL_TEXTURE_2D, 0);
+    stbi_image_free(wall);
+    // Flip image
+    stbi_set_flip_vertically_on_load(true);
+    // Load awesome face texture
+    unsigned char *face = stbi_load("../resources/textures/awesomeface.png", &width, &height, &nrChannels, 0);
+    // Set TEXTURE_1 current active texture
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, face);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    // Unbind texture
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    // Unbind the buffers
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        // Clear window
+        glClear(GL_COLOR_BUFFER_BIT);
+        // Bind the shader program
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+        shader->use();
+        shader->setUniform1i("texture1", 0);
+        shader->setUniform1i("texture2", 1);
+        // Bind the VAO
+        glBindVertexArray(VAO);
+        // Draw the triangle
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    //glDeleteTextures(1, &texture);
+    delete shader;
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
