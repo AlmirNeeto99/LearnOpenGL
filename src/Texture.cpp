4629ddc0bb838a79e5ba1147ae38e0fc5d1e0952
Almir Neto <almirneto338@gmail.com>
2021-08-24 22:08:29 -0300

    Defining and implementing a texture class

---
+++ src/Texture.cpp
@@ -0,0 +1,58 @@
+#include "headers/Texture.hpp"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+Texture::Texture(std::string path)
+{
+    this->data = this->loadImage(path);
+    this->unit = GL_TEXTURE0;
+    glGenTextures(1, &this->id);
+    glBindTexture(GL_TEXTURE_2D, this->id);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, this->width, this->height, 0, GL_RGB, GL_UNSIGNED_BYTE, this->data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+unsigned char *Texture::loadImage(std::string path)
+{
+    return stbi_load(path.c_str(), &this->width, &this->height, &this->nrChannels, 0);
+}
+
+void Texture::unbind()
+{
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+void Texture::bind()
+{
+    glBindTexture(GL_TEXTURE_2D, this->id);
+}
+
+void Texture::useUnit()
+{
+    glActiveTexture(this->unit);
+}
+
+void Texture::setUnit(int unit)
+{
+    this->unit = unit;
+}
+
+int Texture::getId()
+{
+    return this->id;
+}
+
+int Texture::getUnit()
+{
+    return this->unit;
+}
+
+Texture::~Texture(){
+    stbi_image_free(this->data);
+}
---
+++ src/headers/Texture.hpp
@@ -0,0 +1,29 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+#include <string>
+#include "glad/glad.h"
+
+class Texture
+{
+private:
+    unsigned int id;
+    unsigned char *data;
+    int width, height, nrChannels, unit;
+    unsigned char *loadImage(std::string path);
+
+public:
+    Texture(std::string path);
+    ~Texture();
+    // Binders
+    void bind();
+    void unbind();
+    void useUnit();
+    // Getters
+    int getId();
+    int getUnit();
+    // Setters
+    void setUnit(int);
+};
+
+#endif
